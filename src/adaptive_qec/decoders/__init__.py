0bafc60a2f4238f28b697e2d87cf2859c667f998
Prathamsingh069 <pratham.5399.9b@gmail.com>
2026-09-18 02:55:43 +0530

    feat(decoders): define DetectorEdge and DetectorGraph graph representations

--- src/adaptive_qec/decoders/__init__.py
+++ src/adaptive_qec/decoders/__init__.py
@@ -2,3 +2,7 @@
 
 from adaptive_qec.decoders.base import Correction, Decoder, DecoderMetrics
 from adaptive_qec.decoders.mwpm import MWPMDecoder
+from adaptive_qec.decoders.union_find import UnionFindDecoder, UnionFindForest
+from adaptive_qec.decoders.registry import get_decoder, list_decoders, register_decoder
+
+__all__ = [
